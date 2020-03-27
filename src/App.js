7588d5b63f337c4afa0a3f2ebe6e2769186d9100
Michael Sydney Moore <michaelsydneymoore@hotmail.com>
2020-03-27 15:41:17 +0000

    Added the visual basics without functionality

--- src/App.js
+++ src/App.js
@@ -1,16 +1,44 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
-import Header from "./Header";
 
 function App() {
   return (
     <>
-      <Header />
-      <div className="App">
-        <header className="App-header"></header>
-      </div>
+      <RandomArray />
     </>
   );
+}
+
+class RandomArray extends Component {
+  state = { array: [] };
+  componentDidMount() {
+    this.ArrayRandom();
+  }
+  ArrayRandom = () => {
+    let array = [];
+    for (let i = 0; i <= 90; i++) {
+      array.push(i);
+    }
+    this.setState({ array });
+  };
+
+  render() {
+    const array = this.state.array;
+
+    return (
+      <>
+        <div className="container-array">
+          {array.map((number, index) => (
+            <div
+              className="bars"
+              key={index}
+              style={{ height: `${number}px` }}
+            ></div>
+          ))}
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
