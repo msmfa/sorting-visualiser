6923205a58c62623bf49afa07985a66dcc7549a8
Michael Sydney Moore <michaelsydneymoore@hotmail.com>
2020-03-27 17:40:58 +0000

    Added buttons and display UI

--- src/App.js
+++ src/App.js
@@ -11,23 +11,27 @@ function App() {
 
 class RandomArray extends Component {
   state = { array: [] };
+
   componentDidMount() {
     this.ArrayRandom();
   }
   ArrayRandom = () => {
     let array = [];
-    for (let i = 0; i <= 90; i++) {
-      array.push(i);
+    for (let i = 0; i < 100; i++) {
+      array.push(RandomInt(1, 200));
     }
     this.setState({ array });
   };
+
+  OnSubmit = e =>
+    this.setState({ array: this.state.array.sort((a, b) => a - b) });
 
   render() {
     const array = this.state.array;
 
     return (
       <>
-        <div className="container-array">
+        <div className="array-container">
           {array.map((number, index) => (
             <div
               className="bars"
@@ -36,9 +40,15 @@ class RandomArray extends Component {
             ></div>
           ))}
         </div>
+        <button onClick={this.ArrayRandom}>Generate Random Array</button>
+        <button onClick={this.OnSubmit}>Sort Array</button>
       </>
     );
   }
+}
+
+function RandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 export default App;
