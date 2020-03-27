428aa6e8e94acf45be8295e529ac63e1493c34c3
Michael Sydney Moore <michaelsydneymoore@hotmail.com>
2020-03-27 12:32:07 +0000

    Added header

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 import "./App.css";
+import Header from "./Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
+    <>
+      <Header />
+      <div className="App">
+        <header className="App-header"></header>
+      </div>
+    </>
   );
 }
 
---
+++ src/Header.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+function Header() {
+  return <div>This is a Data visualiser App for sorting algorythms</div>;
+}
+export default Header;
