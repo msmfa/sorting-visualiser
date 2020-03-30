1ad067533852c2347a6dcb1a2ae74e9ea9e6ee40
Michael Sydney Moore <michaelsydneymoore@hotmail.com>
2020-03-30 17:59:46 +0100

    Updated Algo

--- src/App.js
+++ src/App.js
@@ -13,18 +13,21 @@ function RandomArray() {
   let array = [1, 9, 3, 9, 6, 4, 8, 7];
 
   const [state, setState] = useState(array);
+
   const generateRandomArray = e => {
-    setState(Array.from({ length: 9 }, () => Math.floor(Math.random() * 9)));
+    setState(
+      Array.from({ length: 10 }, () => Math.floor(Math.random() * 9) + 1)
+    );
   };
 
   function bubbleSortBasic(array) {
-    for (let i = 0; i < array.length; i++) {
-      for (let j = 1; j < array.length; j++) {
-        if (array[j - 1] > array[j]) {
-          let temp = array[i];
-          array[i] = array[j];
-          array[j] = temp;
-        }
+    let len = array.length;
+
+    for (let j = 0; j < len; j++) {
+      if (array[j] > array[j + 1]) {
+        let tmp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = tmp;
       }
     }
     return array;
@@ -32,11 +35,11 @@ function RandomArray() {
 
   const sort = () => {
     setTimeout(function() {
-      console.log(bubbleSortBasic(state));
-      let stage = bubbleSortBasic(state);
-      setState([...state, bubbleSortBasic(state)]);
-    }, 2000);
-    console.log(state);
+      setState([...bubbleSortBasic(state)]);
+      console.log(state);
+    }, 500);
+
+    //If state === state could be a base for a recursive function?
   };
 
   return (
@@ -48,7 +51,9 @@ function RandomArray() {
               height: `${item * 50}px`
             }}
             className="bars"
-          ></li>
+          >
+            {item}
+          </li>
         ))}
       </div>
       <button onClick={generateRandomArray}>Random Array</button>
